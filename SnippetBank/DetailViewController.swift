33a54972598928e3560e1419e63a01082f4f006c
LuizRamos <luizramospe@hotmail.com>
2021-03-25 20:31:55 -0300

    Add the library  Sourcefull

---
+++ SnippetBank/DetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetailViewController.swift
+//  SnippetBank
+//
+//  Created by Luiz on 3/25/21.
+//
+
+import UIKit
+import Sourceful
+
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
