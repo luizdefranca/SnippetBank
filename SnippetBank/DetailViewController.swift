0bd421209b99401e37795629bee7b5cc00c9ad99
LuizRamos <luizramospe@hotmail.com>
2021-03-25 21:08:59 -0300

    Created struct Snnipet and added files utils on project

--- SnippetBank/DetailViewController.swift
+++ SnippetBank/DetailViewController.swift
@@ -11,12 +11,27 @@ import Sourceful
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var syntaxTextView: SyntaxTextView!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-    
+
+    var snippet: Snippet? {
+        didSet {
+            refreshUI()
+        }
+    }
+
+    private func refreshUI() {
+        loadViewIfNeeded()
+        title = snippet?.name ?? "New Snippet"
+        syntaxTextView.text = snippet?.content ?? ""
+    }
+
 
 }
---
+++ SnippetBank/Snippet.swift
@@ -0,0 +1,13 @@
+//
+//  Snippet.swift
+//  SnippetBank
+//
+//  Created by Luiz on 3/25/21.
+//
+
+import Foundation
+
+struct Snippet {
+    var name: String
+    var content: String
+}
